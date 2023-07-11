6b2ae04ad11d2c6fea525920cbbf68529a48ea52
Sabina Fritz <sabinafri@gmail.com>
2023-07-11 20:36:06 +0200

    Zadanie domowe dzień 4

--- wyzwanie21/wyzwanie21/Program.cs
+++ wyzwanie21/wyzwanie21/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+String name = "Sabina";
+char sex = 'F';
+int age = 23;
+
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == 'F' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (sex == 'F' && age >= 30)
+{
+    Console.WriteLine("Kobieta minimum 30 lat");
+}
+else if (sex != 'F')
+{
+    if (age >= 18)
+    {
+        Console.WriteLine("Pełnoletni mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+}
